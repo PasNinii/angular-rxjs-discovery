c1733397f1ca4ac7ae0c114e898473d146a7bca0
Adrien Boulet <adriboulet97@gmail.com>
2020-02-05 15:45:04 +0100

    test

--- sem1/src/app/cinema/movies/tabs/casting/casting.component.ts
+++ sem1/src/app/cinema/movies/tabs/casting/casting.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { MovieService } from '../../../../services/movie.service';
+import { map, expand, scan, take, debounceTime } from 'rxjs/operators';
+import { Credits, Crew, Cast } from './../../../../interfaces/movie/movieInterface';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+
+
 
 @Component({
   selector: 'app-casting',
@@ -9,9 +17,16 @@ export class CastingComponent implements OnInit {
 
   @Input( ) id: number;
 
-  constructor() { }
+  //credits$  : Observable<Credits[]>
+  crew$ : Observable<Crew[]>
+  cast$ : Observable<Cast[]>
+
+  constructor( private router: Router, public dialog: MatDialog, private movieService: MovieService ) { }
 
   ngOnInit() {
+    
+    this.crew$ = this.movieService.getCrew(this.id);
+    this.cast$ = this.movieService.getCast(this.id);
   }
 
 }
--- sem1/src/app/services/movie.service.ts
+++ sem1/src/app/services/movie.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
-import { MovieInterface, MoviesResponse, Genre, GenreResponse, Video, Videos } from '../interfaces/movie/movieInterface';
-
+import { MovieInterface, MoviesResponse, Genre, GenreResponse, Video, Videos, Crew, Credits, Cast } from '../interfaces/movie/movieInterface';
+HttpClient
 
 @Injectable({
   providedIn: 'root'
@@ -39,5 +39,16 @@ export class MovieService {
         ( response ) => response.results
     ) );
   }
-
+  getCrew( crewId: number ): Observable<Crew[]> {
+    return this.http.get<Credits>( `${this.urlMovieHttps}${crewId}/credits?api_key=${this.apiKey}`).pipe(
+      map(
+        (response) => response.crew
+      ));
+  }
+  getCast( crewId: number ): Observable<Cast[]> {
+    return this.http.get<Credits>( `${this.urlMovieHttps}${crewId}/credits?api_key=${this.apiKey}`).pipe(
+      map(
+        (response) => response.cast
+      ));
+  }
 }
